9c2f58bc5094a39d147497efeece4267f11110ec
tomas.fryza <tomas.fryza@gmail.com>
2019-10-02 09:45:53 +0200

    Push button added

--- projects/02-leds/main.c
+++ projects/02-leds/main.c
@@ -19,7 +19,9 @@
 /* Typedef -----------------------------------------------------------*/
 /* Define ------------------------------------------------------------*/
 #define LED_GREEN   PB5
-#define BLINK_DELAY 250
+#define LED_RED     PB0
+#define BTN_BLUE    PD2
+#define BLINK_DELAY 500
 
 /* Variables ---------------------------------------------------------*/
 /* Function prototypes -----------------------------------------------*/
@@ -32,16 +34,25 @@
   */
 int main(void)
 {
-    /* Set output pin to low level */
+    /* Set output pins to low level */
     DDRB |= _BV(LED_GREEN);         /* or: Set to 1 */
+    DDRB |= _BV(LED_RED);
     PORTB &= ~_BV(LED_GREEN);       /* and: Clear to 0 */
+    PORTB &= ~_BV(LED_RED);
+
+    /* Activate internal pull-up resistor */
+    DDRD &= ~_BV(BTN_BLUE);
+    PORTD |= _BV(BTN_BLUE);
 
     /* Infinite loop */
     for (;;)
     {
-        /* Invert LED and delay */
-        PORTB ^= _BV(LED_GREEN);    /* xor: Invert bit */
-        _delay_ms(BLINK_DELAY);     /* Wait for several milisecs */
+        if (bit_is_clear(PIND, BTN_BLUE)) {
+            /* Invert LED and delay */
+            PORTB ^= _BV(LED_GREEN);    /* xor: Invert bit */
+            PORTB ^= _BV(LED_RED);
+            _delay_ms(BLINK_DELAY);     /* Wait for several milisecs */
+        }
     }
 
     return (0);
