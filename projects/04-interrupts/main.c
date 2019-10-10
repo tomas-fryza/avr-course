1064782ca085e5a70ffeaa8a41879ad2d6e495a2
Tomas Fryza <tomas.fryza@gmail.com>
2019-10-10 22:32:13 +0200

    ISR of Timer0 and Timer1 added

--- projects/04-interrupts/main.c
+++ projects/04-interrupts/main.c
@@ -2,37 +2,38 @@
  * ---------------------------------------------------------------------
  * Author:      Tomas Fryza
  *              Dept. of Radio Electronics, Brno Univ. of Technology
- * Created:     2019-09-26
- * Last update: 2019-10-04
+ * Created:     2018-11-28
+ * Last update: 2019-10-11
  * Platform:    ATmega328P, AVR 8-bit Toolchain 3.6.2
  * ---------------------------------------------------------------------
- * Description: Control LEDs using functions from GPIO library.
- * TODO: Create and test functions to control GPIO pin.
- * NOTE: Store gpio.c and gpio.h files in library/src and library/inc
+ * Description: Control LEDs using functions from GPIO and Timer 
+ *              libraries. Do not use delay library any more.
+ * TODO: Create and test functions to control 8-bit Timer/Counter0 and 
+ *       16-bit Timer/Counter1 modules.
+ * NOTE: Store timer.c and timer.h files in library/src and library/inc
  *       folders.
  */
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>
-#include <util/delay.h>
 #include "gpio.h"
+#include "timer.h"
 
 /* Typedef -----------------------------------------------------------*/
 /* Define ------------------------------------------------------------*/
 #define LED_GREEN   PB5
 #define LED_RED     PB0
-#define BTN_BLUE    PD2
-#define BLINK_DELAY 500
 
 /* Variables ---------------------------------------------------------*/
 /* Function prototypes -----------------------------------------------*/
 
 /* Functions ---------------------------------------------------------*/
 /**
-  * Brief:  Main program. Blink two LEDs with a push button.
-  * Input:  None
-  * Return: None
-  */
+ *  Brief:  Main program. Toggle two LEDs with the modules of internal 
+ *          8- and 16-bit timers.
+ *  Input:  None
+ *  Return: None
+ */
 int main(void)
 {
     /* Set output pins of two LEDs to low level */
@@ -40,16 +41,36 @@ int main(void)
     GPIO_config_output(&DDRB, LED_GREEN);
     GPIO_write(&PORTB, LED_GREEN, PIN_LOW);
     /* LED_RED */
+    // TODO: Configure red LED
 
-    /* Configure input pin of push button and enable internal pull-up
-       resistor */
+    /* Set Timer0 */
+    // TODO: Configure Timer0 clock source and enable overflow interrupt
+
+    /* Set Timer1 */
+    // TODO: Configure Timer0 clock source and enable overflow interrupt
+
+    /* Enable interrupts by setting the global interrupt mask */
+    sei();
 
     /* Infinite loop */
     for (;;) {
-        /* Test if push button value is low */
-            /* Invert LED and delay */
-            _delay_ms(BLINK_DELAY);
     }
 
     return (0);
+}
+
+/**
+ *  Brief: Timer/Counter0 overflow interrupt routine. Toggle green LED.
+ */
+ISR(TIMER0_OVF_vect)
+{
+    // TODO: Toggle green LED
+}
+
+/**
+ *  Brief: Timer/Counter1 overflow interrupt routine. Toggle red LED.
+ */
+ISR(TIMER1_OVF_vect)
+{
+    // TODO: Toggle red LED
 }
