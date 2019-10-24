0d11398f9d5fcc1e9e6999dd1a9db78a9908de15
Tomas Fryza <tomas.fryza@gmail.com>
2019-10-24 10:54:01 +0200

    New project 06-lcd added

---
+++ projects/06-lcd/main.c
@@ -0,0 +1,64 @@
+/*
+ * ---------------------------------------------------------------------
+ * Author:      Tomas Fryza
+ *              Dept. of Radio Electronics, Brno Univ. of Technology
+ * Created:     2018-10-16
+ * Last update: 2019-10-24
+ * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
+ * ---------------------------------------------------------------------
+ * Description:
+ *    Decimal counter with data display on LCD display.
+ * 
+ * Note:
+ *    Modified version of Peter Fleury's LCD library with R/W pin 
+ *    connected to GND. Newline symbol "\n" is not implemented, use
+ *    lcd_gotoxy() function instead.
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "timer.h"
+#include "lcd.h"
+
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+ *  Brief:  Main program. Shows decimal values ​​on LCD display.
+ *  Input:  None
+ *  Return: None
+ */
+int main(void)
+{
+    /* LCD display
+     * TODO: Initialize display and test different types of cursor */
+    lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+
+    // Display string without auto linefeed
+    lcd_puts("LCD testing");
+
+    /* Timer1
+     * TODO: Configure Timer1 clock source and enable overflow 
+     *       interrupt */
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    for (;;) {
+    }
+
+    return (0);
+}
+
+/**
+ *  Brief: Timer1 overflow interrupt routine. Increment counter value.
+ */
+ISR(TIMER1_OVF_vect)
+{
+    // TODO: Increment counter value form 0 to 99
+}
