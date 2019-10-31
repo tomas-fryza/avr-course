31438cf42b3a93757d3b54efae9459104b7e623e
Tomas Fryza <tomas.fryza@gmail.com>
2019-10-31 12:32:56 +0100

    New project 07-uart created

---
+++ projects/07-uart/main.c
@@ -0,0 +1,86 @@
+/*
+ * ---------------------------------------------------------------------
+ * Author:      Tomas Fryza
+ *              Dept. of Radio Electronics, Brno Univ. of Technology
+ * Created:     2018-10-23
+ * Last update: 2019-11-01
+ * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
+ * ---------------------------------------------------------------------
+ * Description:
+ *    Analog-to-Digital Conversion with data output on LCD display and
+ *    UART.
+ * 
+ * Note:
+ *    Peter Fleury's UART library.
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>             // itoa() function
+#include "timer.h"
+#include "lcd.h"
+#include "uart.h"
+
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+/** @brief Define UART buad rate. */
+#define UART_BAUD_RATE 9600
+
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+ *  Brief:  Main program. Read ADC value and transmit via UART.
+ *  Input:  None
+ *  Return: None
+ */
+int main(void)
+{
+    // LCD display
+    lcd_init(LCD_DISP_ON);
+
+    // UART: asynchronous, 8-bit data, no parity, 1-bit stop
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Display string without auto linefeed
+    lcd_puts("LCD testing");
+    // Put string to ringbuffer for transmitting via UART.
+    uart_puts("UART testing");
+
+    /* ADC
+     * TODO: Configure ADC reference, clock source and enable 
+     *       conversion complete interrupt */
+
+    /* Timer1
+     * TODO: Configure Timer1 clock source and enable overflow 
+     *       interrupt */
+
+    // Infinite loop
+    for (;;) {
+    }
+
+    // Will never reach this
+    return (0);
+}
+
+/**
+ *  Brief: Timer1 overflow interrupt routine. Start ADC conversion.
+ */
+ISR(TIMER1_OVF_vect)
+{
+    // TODO: Start ADC conversion
+}
+
+/**
+ *  Brief: ADC complete interrupt routine. Update LCD and UART 
+ *         transmiter.
+ */
+ISR(ADC_vect)
+{
+    // TODO: Read ADC value, update LCD and UART transmiter
+}
