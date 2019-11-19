45d88489e0f07f3496eb3002a68939228a2835f6
Tomas Fryza <tomas.fryza@gmail.com>
2019-11-19 07:26:45 +0100

    Functions rewrote to defines

--- projects/library/inc/timer.h
+++ projects/library/inc/timer.h
@@ -3,67 +3,59 @@
 
 /***********************************************************************
  * 
- * Title:    Timer library
- * Author:   Tomas Fryza, Brno University of Technology, Czechia
- * Software: avr-gcc, tested with avr-gcc 4.9.2
- * Hardware: Any AVR
+ * Timer library for AVR-GCC.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
  * Copyright (c) 2019 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  *
  **********************************************************************/
 
 /**
- *  @file      timer.h
- *  @brief     Timer library for AVR-GCC.
+ *  @file  timer.h
+ *  @brief Timer library for AVR-GCC.
  *
- *  @details   The library contains functions for controlling the timer
- *             modules
+ *  @details
+ *  The library contains settings for controlling the timer modules.
  *
- *  @author    Tomas Fryza, Brno University of Technology, Czechia
- *  @date      2019-11-05
- *  @version   v1.0
- *  @copyright (c) 2019 Tomas Fryza, MIT License
- *
- *  @note      Based on Microchip Atmel ATmega328P manual
+ *  @note
+ *  Based on Microchip Atmel ATmega328P manual.
+ * 
+ *  @copyright (c) 2019 Tomas Fryza
+ *  Dept. of Radio Electronics, Brno University of Technology, Czechia
+ *  This work is licensed under the terms of the MIT license.
  */
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>
 
 /* Define ------------------------------------------------------------*/
-/** @brief Defines the designation of each timer. */
-#define TIM0    0
-#define TIM1    1
-#define TIM2    2
+/** @brief Defines prescaler values for Timer0.
+ *  @note  F_CPU = 16 MHz */
+// TODO: Define Prescaler values for Timer1
 
-/** @brief Defines individual prescaler values for timers. */
-#define TIM_PRESC_1     0
-#define TIM_PRESC_8     1
-#define TIM_PRESC_32    2
-#define TIM_PRESC_64    3
-#define TIM_PRESC_128   4
-#define TIM_PRESC_256   5
-#define TIM_PRESC_1024  6
-#define TIM_STOP        7
+/** @brief Defines prescaler values for Timer1.
+ *  @note  F_CPU = 16 MHz */
+#define TIM1_stop()             TCCR1B &= ~(_BV(CS12) | _BV(CS11) | _BV(CS10));
+#define TIM1_overflow_4ms()     TCCR1B &= ~(_BV(CS12) | _BV(CS11)); TCCR1B |= _BV(CS10);
+#define TIM1_overflow_33ms()    TCCR1B &= ~(_BV(CS12) | _BV(CS10)); TCCR1B |= _BV(CS11);
+#define TIM1_overflow_262ms()   TCCR1B &= ~(_BV(CS12)); TCCR1B |= _BV(CS11) | _BV(CS10);
+#define TIM1_overflow_1s()      TCCR1B &= ~(_BV(CS11) | _BV(CS10)); TCCR1B |= _BV(CS12);
+#define TIM1_overflow_4s()      TCCR1B &= ~(_BV(CS11)); TCCR1B |= _BV(CS12) | _BV(CS10);
 
-/** @brief Defines individual interrupt modes for timers. */
-#define TIM_OVERFLOW_DISABLE    0
-#define TIM_OVERFLOW_ENABLE     1
+/** @brief Defines prescaler values for Timer2.
+ *  @note  F_CPU = 16 MHz */
+// TODO: Define Prescaler values for Timer2
 
-/* Function prototypes -----------------------------------------------*/
-/**
- *  @brief Xxx.
- *  @param xxx - Xxxx
- */
-void TIM_config_prescaler(uint8_t timer_name,
-                          uint8_t presc_val);
+/** @brief Defines interrupt modes for Timer0. */
+// TODO: Define interrupt modes for Timer0
 
-/**
- *  @brief Xxx.
- *  @param xxx - Xxxx
- */
-void TIM_config_interrupt(uint8_t timer_name,
-                          uint8_t interr_mode);
+/** @brief Defines interrupt modes for Timer1. */
+#define TIM1_overflow_interrupt_enable()   TIMSK1 |= _BV(TOIE1);
+#define TIM1_overflow_interrupt_disable()   TIMSK1 &= ~_BV(TOIE1);
+
+/** @brief Defines interrupt modes for Timer2. */
+// TODO: Define interrupt modes for Timer2
 
 #endif /* TIMER_H_INCLUDED */
