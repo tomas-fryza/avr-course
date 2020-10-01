4a977b9210dd765300a9d721f516c9b6fec9dbf9
Tomas Fryza <tomas.fryza@gmail.com>
2020-10-01 13:46:42 +0200

    [LIB] Libraries copied from Examples to Labs

---
+++ Labs/library/include/lcd_definitions.h
@@ -0,0 +1,16 @@
+#ifndef LCD_DEFINITIONS_H
+#define LCD_DEFINITIONS_H
+
+/* Arduino Uno LCD Keypad Shield */
+#define LCD_PORT        PORTD
+#define LCD_DATA0_PIN   PD4
+#define LCD_DATA1_PIN   PD5
+#define LCD_DATA2_PIN   PD6
+#define LCD_DATA3_PIN   PD7
+#define LCD_RS_PORT     PORTB
+#define LCD_RS_PIN      PB0
+#define LCD_E_PORT      PORTB
+#define LCD_E_PIN       PB1
+/* R/W pin is connected to GND */
+
+#endif // ifndef LCD_DEFINITIONS_H
