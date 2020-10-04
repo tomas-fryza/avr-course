edeeea29b8ca92dbffc939c82d743aeb0da34bae
tomas-fryza <48966045+tomas-fryza@users.noreply.github.com>
2020-10-04 20:29:17 +0200

    Update gpio.c

--- Labs/library/gpio.c
+++ Labs/library/gpio.c
@@ -25,7 +25,7 @@ void GPIO_config_output(volatile uint8_t *reg_name, uint8_t  pin_num)
 void GPIO_config_input_pullup(volatile uint8_t *reg_name, uint8_t pin_num)
 {
     *reg_name = *reg_name & ~(1<<pin_num);  // Data Direction Register
-    *reg_name++;    // Change pointer to Data Register
+    *reg_name++;                    // Change pointer to Data Register
     *reg_name = *reg_name | (1<<pin_num);   // Data Register
 }
 
