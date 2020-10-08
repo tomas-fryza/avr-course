5d40cf499a0e4b36c0a9c8f4051a4c4bad090604
Tomas Fryza <tomas.fryza@gmail.com>
2020-10-08 11:24:20 +0200

    [LIB] Template for timer.h header file created

---
+++ Labs/library/include/timer.h
@@ -0,0 +1,52 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+/***********************************************************************
+ * 
+ * Timer library for AVR-GCC.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2019-2020 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/**
+ * @file  timer.h
+ * @brief Timer library for AVR-GCC.
+ *
+ * @details
+ * The library contains macros for controlling the timer modules.
+ *
+ * @note
+ * Based on Microchip Atmel ATmega328P manual and no source file is 
+ * needed for the library.
+ * 
+ * @copyright (c) 2019-2020 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+
+/* Defines -----------------------------------------------------------*/
+/**
+ * @brief Defines prescaler CPU frequency values for Timer/Counter1.
+ * @note  F_CPU = 16 MHz
+ */
+#define TIM1_stop()             TCCR1B &= ~((1<<CS12) | (1<<CS11) | (1<<CS10));
+#define TIM1_overflow_4ms()     TCCR1B &= ~((1<<CS12) | (1<<CS11)); TCCR1B |= (1<<CS10);
+#define TIM1_overflow_33ms()    TCCR1B &= ~((1<<CS12) | (1<<CS10)); TCCR1B |= (1<<CS11);
+#define TIM1_overflow_262ms()   TCCR1B &= ~(1<<CS12); TCCR1B |= (1<<CS11) | (1<<CS10);
+#define TIM1_overflow_1s()      TCCR1B &= ~((1<<CS11) | (1<<CS10)); TCCR1B |= (1<<CS12);
+#define TIM1_overflow_4s()      TCCR1B &= ~(1<<CS11); TCCR1B |= (1<<CS12) | (1<<CS10);
+
+/**
+ * @brief Defines interrupt enable/disable modes for Timer/Counter1.
+ */
+#define TIM1_overflow_interrupt_enable()    TIMSK1 |= (1<<TOIE1);
+#define TIM1_overflow_interrupt_disable()   TIMSK1 &= ~(1<<TOIE1);
+
+#endif
