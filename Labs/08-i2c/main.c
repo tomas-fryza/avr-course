a28d8c086d8efd23062702f6fe370d9c2abadd99
tomas.fryza <tomas.fryza@gmail.com>
2020-11-13 13:48:53 +0100

    [C] Template for I2C scanner created

---
+++ Labs/08-i2c/main.c
@@ -0,0 +1,115 @@
+/***********************************************************************
+ * 
+ * The I2C bus scanner detects the addresses of the modules that are 
+ * connected to the SDA and SCL signals. A simple description of FSM is 
+ * used.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2017-2020 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
+/* Defines -----------------------------------------------------------*/
+#ifndef F_CPU
+#define F_CPU 16000000
+#endif
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <stdlib.h>         // C library. Needed for conversion function
+#include "uart.h"           // Peter Fleury's UART library
+#include "twi.h"            // TWI library for AVR-GCC
+
+/* Variables ---------------------------------------------------------*/
+typedef enum {              // FSM declaration
+    STATE_IDLE = 1,
+    STATE_SEND,
+    STATE_ACK
+} state_t;
+
+/* Function definitions ----------------------------------------------*/
+/**
+ * Main function where the program execution begins. Use Timer/Counter1
+ * and send I2C (TWI) address every 33 ms. Send information about 
+ * scanning process to UART.
+ */
+int main(void)
+{
+    // Initialize I2C (TWI)
+    twi_init();
+
+    // Initialize UART to asynchronous, 8N1, 9600
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
+
+    // Configure 16-bit Timer/Counter1 to update FSM
+    // Enable interrupt and set the overflow prescaler to 33 ms
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Put strings to ringbuffer for transmitting via UART
+    uart_puts("\r\nScan I2C-bus for devices:\r\n");
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**
+ * ISR starts when Timer/Counter1 overflows. Update Finite State Machine
+ * and test I2C slave addresses between 8 and 119.
+ */
+ISR(TIMER1_OVF_vect)
+{
+    static state_t state = STATE_IDLE;  // Current FSM state value
+    static uint8_t addr = 7;            // I2C slave address
+    uint8_t result = 1;                 // ACK result from the bus
+    char uart_string[2] = "00"; // String for converting numbers by itoa()
+
+    // FSM
+    switch (state)
+    {
+    // Increment I2C slave address
+    case STATE_IDLE:
+        addr++;
+        // If slave address is between 8 and 119 then move to the next state
+
+        break;
+    
+    // Transmit I2C slave address and get result
+    case STATE_SEND:
+        //  7  6  5  4  3  2  1  0
+        // a6 a5 a4 a3 a2 a1 a0 R/W
+        result = twi_start((addr<<1) + TWI_WRITE);
+        twi_stop();
+        /* Test result from I2C bus. If it is 0 move to ACK state, 
+         * otherwise move to IDLE */
+
+        break;
+
+    // A module connected to the bus was found
+    case STATE_ACK:
+        // Send info about active I2C address to UART and move to IDLE
+
+        break;
+
+    // If something unexpected happens, move to IDLE
+    default:
+        state = STATE_IDLE;
+        break;
+    }
+}
