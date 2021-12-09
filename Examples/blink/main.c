91113e90409d842d22e31c9f7bb509595788187b
tomas-fryza <48966045+tomas-fryza@users.noreply.github.com>
2021-12-09 14:17:31 +0100

    Update main.c

--- Examples/blink/main.c
+++ Examples/blink/main.c
@@ -10,26 +10,13 @@
  **********************************************************************/
 
 /* Defines -----------------------------------------------------------*/
-/* The preprocessor will process directives that are inserted into the C
- * source code. These directives allow additional actions to be taken on
- * the C source code before it is compiled into object code. Directives
- * are not part of the C language itself.
- *
- * Preprocessor directives begin with a pound (#) symbol and may have 
- * several arguments. Do NOT put a semicolon character at the end of 
- * directives. This is a common mistake.
- */
 #define LED_GREEN   PB5 // AVR pin where green LED is connected
 #define SHORT_DELAY 250 // Delay in milliseconds
-#ifndef F_CPU           // Preprocessor directive allows for conditional
-                        // compilation. The #ifndef means "if not defined".
+#ifndef F_CPU
 # define F_CPU 16000000 // CPU frequency in Hz required for delay
-#endif                  // The #ifndef directive must be closed by #endif
+#endif
 
 /* Includes ----------------------------------------------------------*/
-/* Include another C language file into the current file at the location
- * of the #include statement prior to compiling the source code.
- */
 #include <util/delay.h> // Functions for busy-wait delay loops
 #include <avr/io.h>     // AVR device-specific IO definitions
 
@@ -42,11 +29,9 @@
 int main(void)
 {
     // Set pin as output in Data Direction Register
-    // DDRB = DDRB or 0010 0000
     DDRB = DDRB | (1<<LED_GREEN);
 
     // Set pin LOW in Data Register (LED off)
-    // PORTB = PORTB and 1101 1111
     PORTB = PORTB & ~(1<<LED_GREEN);
 
     // Infinite loop
@@ -56,7 +41,6 @@ int main(void)
         _delay_ms(SHORT_DELAY);
 
         // Invert LED in Data Register
-        // PORTB = PORTB xor 0010 0000
         PORTB = PORTB ^ (1<<LED_GREEN);
     }
 
