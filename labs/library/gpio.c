821206228a578e52fb84b3a34a7977ec1b80b3b8
Tomas Fryza <tomas.fryza@gmail.com>
2022-10-02 20:45:32 +0200

    Two functions added: write_high, read

--- labs/library/gpio.c
+++ labs/library/gpio.c
@@ -3,66 +3,99 @@
  * GPIO library for AVR-GCC.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2019-Present Tomas Fryza
+ * Copyright (c) 2019 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  *
  **********************************************************************/
 
+
 /* Includes ----------------------------------------------------------*/
 #include "gpio.h"
 
+
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
- * Function: GPIO_config_output()
- * Purpose:  Configure one output pin in Data Direction Register.
- * Input:    reg_name - Address of Data Direction Register, such as &DDRB
- *           pin_num - Pin designation in the interval 0 to 7
+ * Function: GPIO_mode_output()
+ * Purpose:  Configure one output pin.
+ * Input(s): reg - Address of Data Direction Register, such as &DDRB
+ *           pin - Pin designation in the interval 0 to 7
  * Returns:  none
  **********************************************************************/
-void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num)
+void GPIO_mode_output(volatile uint8_t *reg, uint8_t pin)
 {
-    *reg_name = *reg_name | (1<<pin_num);
+    *reg = *reg | (1<<pin);
 }
 
-/**********************************************************************
- * Function: GPIO_config_input_nopull()
- **********************************************************************/
 
 /**********************************************************************
- * Function: GPIO_config_input_pullup()
+ * Function: GPIO_mode_input_pullup()
  * Purpose:  Configure one input pin and enable pull-up.
- * Input:    reg_name - Address of Data Direction Register, such as &DDRB
- *           pin_num - Pin designation in the interval 0 to 7
+ * Input(s): reg - Address of Data Direction Register, such as &DDRB
+ *           pin - Pin designation in the interval 0 to 7
  * Returns:  none
  **********************************************************************/
-void GPIO_config_input_pullup(volatile uint8_t *reg_name, uint8_t pin_num)
+void GPIO_mode_input_pullup(volatile uint8_t *reg, uint8_t pin)
 {
-    *reg_name = *reg_name & ~(1<<pin_num);  // Data Direction Register
-    reg_name++;                     // Change pointer to Data Register
-    *reg_name = *reg_name | (1<<pin_num);   // Data Register
+    *reg = *reg & ~(1<<pin);  // Data Direction Register
+    reg++;                    // Change pointer to Data Register
+    *reg = *reg | (1<<pin);   // Data Register
 }
+
 
 /**********************************************************************
  * Function: GPIO_write_low()
- * Purpose:  Write one pin to a low value.
- * Input:    reg_name - Address of Port Register, such as &PORTB
- *           pin_num - Pin designation in the interval 0 to 7
+ * Purpose:  Write one pin to low value.
+ * Input(s): reg - Address of Port Register, such as &PORTB
+ *           pin - Pin designation in the interval 0 to 7
  * Returns:  none
  **********************************************************************/
-void GPIO_write_low(volatile uint8_t *reg_name, uint8_t pin_num)
+void GPIO_write_low(volatile uint8_t *reg, uint8_t pin)
 {
-    *reg_name = *reg_name & ~(1<<pin_num);
+    *reg = *reg & ~(1<<pin);
 }
+
 
 /**********************************************************************
  * Function: GPIO_write_high()
+ * Purpose:  Write one pin to high value.
+ * Input(s): reg - Address of Port Register, such as &PORTB
+ *           pin - Pin designation in the interval 0 to 7
+ * Returns:  none
  **********************************************************************/
+void GPIO_write_high(volatile uint8_t *reg, uint8_t pin)
+{
+    *reg = *reg | (1<<pin);
+}
 
-/**********************************************************************
- * Function: GPIO_toggle()
- **********************************************************************/
 
 /**********************************************************************
  * Function: GPIO_read()
+ * Purpose:  Read a value from input pin.
+ * Input(s): reg - Address of Pin Register, such as &PINB
+ *           pin - Pin designation in the interval 0 to 7
+ * Returns:  Pin value
+ **********************************************************************/
+uint8_t GPIO_read(volatile uint8_t *reg, uint8_t pin)
+{
+    uint8_t temp;
+
+    temp = *reg & (1<<pin);
+
+    if (temp != 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+/**********************************************************************
+ * Function: GPIO_mode_input_nopull()
+ **********************************************************************/
+
+
+/**********************************************************************
+ * Function: GPIO_write_toggle()
  **********************************************************************/
--- labs/library/include/gpio.h
+++ labs/library/include/gpio.h
@@ -6,7 +6,7 @@
  * GPIO library for AVR-GCC.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2019-Present Tomas Fryza
+ * Copyright (c) 2019 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  *
@@ -25,7 +25,7 @@
  *       (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2.
  * @author Tomas Fryza, Dept. of Radio Electronics, Brno University 
  *         of Technology, Czechia
- * @copyright (c) 2019-Present Tomas Fryza, This work is licensed under 
+ * @copyright (c) 2019 Tomas Fryza, This work is licensed under 
  *                the terms of the MIT license
  * @{
  */
@@ -37,48 +37,55 @@
 
 /* Function prototypes -----------------------------------------------*/
 /**
- * @brief  Configure one output pin in Data Direction Register.
- * @param  reg_name Address of Data Direction Register, such as &DDRB
- * @param  pin_num  Pin designation in the interval 0 to 7
+ * @brief  Configure one output pin.
+ * @param  reg Address of Data Direction Register, such as &DDRB
+ * @param  pin Pin designation in the interval 0 to 7
  * @return none
  */
-void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_mode_output(volatile uint8_t *reg, uint8_t pin);
+
+
+/**
+ * @brief  Configure one input pin and enable pull-up.
+ * @param  reg Address of Data Direction Register, such as &DDRB
+ * @param  pin Pin designation in the interval 0 to 7
+ * @return none
+ */
+void GPIO_mode_input_pullup(volatile uint8_t *reg, uint8_t pin);
+
+
+/**
+ * @brief  Write one pin to low value.
+ * @param  reg Address of Port Register, such as &PORTB
+ * @param  pin Pin designation in the interval 0 to 7
+ * @return none
+ */
+void GPIO_write_low(volatile uint8_t *reg, uint8_t pin);
+
+
+/**
+ * @brief  Write one pin to high value.
+ * @param  reg Address of Port Register, such as &PORTB
+ * @param  pin Pin designation in the interval 0 to 7
+ * @return none
+ */
+void GPIO_write_high(volatile uint8_t *reg, uint8_t pin);
+
+
+/**
+ * @brief  Read a value from input pin.
+ * @param  reg Address of Pin Register, such as &PINB
+ * @param  pin Pin designation in the interval 0 to 7
+ * @return Pin value
+ */
+uint8_t GPIO_read(volatile uint8_t *reg, uint8_t pin);
 
 
 /* GPIO_config_input_nopull */
 
 
-/**
- * @brief  Configure one input pin and enable pull-up.
- * @param  reg_name Address of Data Direction Register, such as &DDRB
- * @param  pin_num  Pin designation in the interval 0 to 7
- * @return none
- */
-void GPIO_config_input_pullup(volatile uint8_t *reg_name, uint8_t pin_num);
-
-
-/**
- * @brief  Write one pin to a low value.
- * @param  reg_name Address of Port Register, such as &PORTB
- * @param  pin_num  Pin designation in the interval 0 to 7
- * @return none
- */
-void GPIO_write_low(volatile uint8_t *reg_name, uint8_t pin_num);
-
-
-/* GPIO_write_high */
-
-
 /* GPIO_toggle */
 
-
-/**
- * @brief  Read a value from input pin.
- * @param  reg_name Address of Pin Register, such as &PINB
- * @param  pin_num  Pin designation in the interval 0 to 7
- * @return Pin value
- */
-uint8_t GPIO_read(volatile uint8_t *reg_name, uint8_t pin_num);
 
 /** @} */
 
