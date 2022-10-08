ca47bfa2beb74487f19a7bace1d1f0e5e29f2c8a
tomas-fryza <tomas.fryza@gmail.com>
2022-10-08 11:03:35 +0200

    Timer overflow example created

---
+++ solutions/lab3-interrupts_timer/include/timer.h
@@ -0,0 +1,78 @@
+#ifndef TIMER_H
+# define TIMER_H
+
+/***********************************************************************
+ * 
+ * Timer library for AVR-GCC.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2019 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/**
+ * @file 
+ * @defgroup fryza_timer Timer Library <timer.h>
+ * @code #include "timer.h" @endcode
+ *
+ * @brief Timer library for AVR-GCC.
+ *
+ * The library contains macros for controlling the timer modules.
+ *
+ * @note Based on Microchip Atmel ATmega328P manual and no source file
+ *       is needed for the library.
+ * @author Tomas Fryza, Dept. of Radio Electronics, Brno University 
+ *         of Technology, Czechia
+ * @copyright (c) 2019 Tomas Fryza, This work is licensed under 
+ *                the terms of the MIT license
+ * @{
+ */
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+
+
+/* Defines -----------------------------------------------------------*/
+/**
+ * @name  Definitions for 16-bit Timer/Counter1
+ * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 16, F_CPU = 16 MHz
+ */
+/** @brief Stop timer, prescaler 000 --> STOP */
+#define TIM1_stop()           TCCR1B &= ~((1<<CS12) | (1<<CS11) | (1<<CS10));
+/** @brief Set overflow 4ms, prescaler 001 --> 1 */
+#define TIM1_overflow_4ms()   TCCR1B &= ~((1<<CS12) | (1<<CS11)); TCCR1B |= (1<<CS10);
+/** @brief Set overflow 33ms, prescaler 010 --> 8 */
+#define TIM1_overflow_33ms()  TCCR1B &= ~((1<<CS12) | (1<<CS10)); TCCR1B |= (1<<CS11);
+/** @brief Set overflow 262ms, prescaler 011 --> 64 */
+#define TIM1_overflow_262ms() TCCR1B &= ~(1<<CS12); TCCR1B |= (1<<CS11) | (1<<CS10);
+/** @brief Set overflow 1s, prescaler 100 --> 256 */
+#define TIM1_overflow_1s()    TCCR1B &= ~((1<<CS11) | (1<<CS10)); TCCR1B |= (1<<CS12);
+/** @brief Set overflow 4s, prescaler // 101 --> 1024 */
+#define TIM1_overflow_4s()    TCCR1B &= ~(1<<CS11); TCCR1B |= (1<<CS12) | (1<<CS10);
+
+/** @brief Enable overflow interrupt, 1 --> enable */
+#define TIM1_overflow_interrupt_enable()  TIMSK1 |= (1<<TOIE1);
+/** @brief Disable overflow interrupt, 0 --> disable */
+#define TIM1_overflow_interrupt_disable() TIMSK1 &= ~(1<<TOIE1);
+
+
+/**
+ * @name  Definitions for 8-bit Timer/Counter0
+ * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
+ */
+// WRITE YOUR CODE HERE
+
+
+/**
+ * @name  Definitions for 8-bit Timer/Counter2
+ * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
+ */
+// WRITE YOUR CODE HERE
+
+
+/** @} */
+
+#endif
---
+++ solutions/lab3-interrupts_timer/src/main.c
@@ -0,0 +1,68 @@
+/***********************************************************************
+ * 
+ * Blink two LEDs using functions from GPIO and Timer libraries. Do not 
+ * use delay library any more.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2018 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
+/* Defines -----------------------------------------------------------*/
+#define LED_GREEN PB5  // Arduino Uno on-board LED
+#define LED_RED PB0    // External active-low LED
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include <timer.h>          // Timer library for AVR-GCC
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Toggle two LEDs using the internal 8- and 16-bit 
+ *           Timer/Counter.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Set pins where LEDs are connected as output
+    GPIO_mode_output(&DDRB, LED_GREEN);
+    GPIO_mode_output(&DDRB, LED_RED);
+
+    // Configuration of 16-bit Timer/Counter1 for LED blinking
+    // Set the overflow prescaler to 262 ms and enable interrupt
+    TIM1_overflow_256();
+    TIM1_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Toggle on-board LED.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    PORTB = PORTB ^ (1<<LED_GREEN);
+}
