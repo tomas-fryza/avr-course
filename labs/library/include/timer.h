dac6985d787dbc76cb7f94fbbe80de642f67373c
Tomas Fryza <tomas.fryza@gmail.com>
2022-10-10 08:47:28 +0200

    minor comments

--- labs/library/include/timer.h
+++ labs/library/include/timer.h
@@ -6,7 +6,7 @@
  * Timer library for AVR-GCC.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2019-Present Tomas Fryza
+ * Copyright (c) 2019 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  *
@@ -25,7 +25,7 @@
  *       is needed for the library.
  * @author Tomas Fryza, Dept. of Radio Electronics, Brno University 
  *         of Technology, Czechia
- * @copyright (c) 2019-Present Tomas Fryza, This work is licensed under 
+ * @copyright (c) 2019 Tomas Fryza, This work is licensed under 
  *                the terms of the MIT license
  * @{
  */
@@ -52,10 +52,12 @@
 #define TIM1_overflow_1s()    TCCR1B &= ~((1<<CS11) | (1<<CS10)); TCCR1B |= (1<<CS12);
 /** @brief Set overflow 4s, prescaler // 101 --> 1024 */
 #define TIM1_overflow_4s()    TCCR1B &= ~(1<<CS11); TCCR1B |= (1<<CS12) | (1<<CS10);
+
 /** @brief Enable overflow interrupt, 1 --> enable */
 #define TIM1_overflow_interrupt_enable()  TIMSK1 |= (1<<TOIE1);
 /** @brief Disable overflow interrupt, 0 --> disable */
 #define TIM1_overflow_interrupt_disable() TIMSK1 &= ~(1<<TOIE1);
+
 
 /**
  * @name  Definitions for 8-bit Timer/Counter0
@@ -63,11 +65,13 @@
  */
 // WRITE YOUR CODE HERE
 
+
 /**
  * @name  Definitions for 8-bit Timer/Counter2
  * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
  */
 // WRITE YOUR CODE HERE
+
 
 /** @} */
 
