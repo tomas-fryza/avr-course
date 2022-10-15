dc626edb1eca04e23d9321dfb80dbd036a04c416
Tomas Fryza <tomas.fryza@gmail.com>
2022-10-15 15:02:22 +0200

    Comments updated

--- labs/library/include/lcd_definitions.h
+++ labs/library/include/lcd_definitions.h
@@ -8,7 +8,7 @@
  * "lcd.h" (see Peter Fleury's LCD library for HD44780 based LCDs).
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2019-Present Tomas Fryza, Peter Fleury
+ * Copyright (c) 2019 Tomas Fryza, Peter Fleury
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  *
@@ -27,10 +27,11 @@
  *
  * @author Tomas Fryza, Peter Fleury, Dept. of Radio Electronics,
  *         Brno University of Technology, Czechia
- * @copyright (c) 2019-Present Tomas Fryza, Peter Fleury, This work is 
+ * @copyright (c) 2019 Tomas Fryza, Peter Fleury, This work is 
  *                licensed under the terms of the MIT license
  * @{
  */
+
 
 /* Defines -----------------------------------------------------------*/
 /**
@@ -41,8 +42,9 @@
  * override the default definitions from lcd.h. Add -D_LCD_DEFINITIONS_FILE
  * to the CDEFS section in the Makefile.
  */
-#define LCD_LINES 2 /**< @brief Number of visible lines of the display */
-#define LCD_DISP_LENGTH 16 /**< @brief Visibles characters per line of the display */
+#define LCD_LINES 2  /**< @brief Number of visible lines of the display */
+#define LCD_DISP_LENGTH 16  /**< @brief Visibles characters per line of the display */
+
 
 /**
  * @name Definitions for 4-bit IO mode
@@ -61,15 +63,16 @@
 #define LCD_DATA1_PORT  LCD_PORT
 #define LCD_DATA2_PORT  LCD_PORT
 #define LCD_DATA3_PORT  LCD_PORT
-#define LCD_DATA0_PIN   PD4 /**< @brief Pin for HD44780 data pin D4 */
-#define LCD_DATA1_PIN   PD5 /**< @brief Pin for HD44780 data pin D5 */
-#define LCD_DATA2_PIN   PD6 /**< @brief Pin for HD44780 data pin D6 */
-#define LCD_DATA3_PIN   PD7 /**< @brief Pin for HD44780 data pin D7 */
+#define LCD_DATA0_PIN   PD4  /**< @brief Pin for HD44780 data pin D4 */
+#define LCD_DATA1_PIN   PD5  /**< @brief Pin for HD44780 data pin D5 */
+#define LCD_DATA2_PIN   PD6  /**< @brief Pin for HD44780 data pin D6 */
+#define LCD_DATA3_PIN   PD7  /**< @brief Pin for HD44780 data pin D7 */
 #define LCD_RS_PORT     PORTB
 #define LCD_RS_PIN      PB0
 #define LCD_E_PORT      PORTB
 #define LCD_E_PIN       PB1
 // R/W pin is connected to GND on LCD Keypad Shield
+
 
 /** @} */
 
