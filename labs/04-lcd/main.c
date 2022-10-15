d8a1c59127b58f9a4fc7ec209e690df531087a99
Tomas Fryza <tomas.fryza@gmail.com>
2022-10-15 15:01:20 +0200

    LCD connection description added; comments updated

--- labs/04-lcd/main.c
+++ labs/04-lcd/main.c
@@ -1,39 +1,57 @@
 /***********************************************************************
  * 
- * Stopwatch with LCD display output.
- * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ * Stopwatch by Timer/Counter2 on the Liquid Crystal Display (LCD)
+
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
- * Copyright (c) 2017-Present Tomas Fryza
+ * Copyright (c) 2017 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
+ * 
+ * Components:
+ *   16x2 character LCD with parallel interface
+ *     VSS  - GND (Power supply ground)
+ *     VDD  - +5V (Positive power supply)
+ *     Vo   - (Contrast)
+ *     RS   - PB0 (Register Select: High for Data transfer, Low for Instruction transfer)
+ *     RW   - GND (Read/Write signal: High for Read mode, Low for Write mode)
+ *     E    - PB1 (Read/Write Enable: High for Read, falling edge writes data to LCD)
+ *     D3:0 - NC (Data bits 3..0, Not Connected)
+ *     D4   - PD4 (Data bit 4)
+ *     D5   - PD5 (Data bit 5)
+ *     D6   - PD6 (Data bit 3)
+ *     D7   - PD7 (Data bit 2)
+ *     A+K  - Back-light enabled by PB2
  * 
  **********************************************************************/
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
 #include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
-#include <stdlib.h>         // C library. Needed for conversion function
+#include <gpio.h>           // GPIO library for AVR-GCC
 #include "timer.h"          // Timer library for AVR-GCC
-#include "lcd.h"            // Peter Fleury's LCD library
+#include <lcd.h>            // Peter Fleury's LCD library
+#include <stdlib.h>         // C library. Needed for number conversions
+
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Update stopwatch value on LCD display when 8-bit 
+ * Purpose:  Update stopwatch value on LCD screen when 8-bit 
  *           Timer/Counter2 overflows.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
     // Initialize LCD display
-    lcd_init(LCD_DISP_ON);
+    lcd_init(LCD_DISP_ON_BLINK);
 
-    // Put string(s) at LCD display
-    lcd_gotoxy(1, 0);
+    // Put string(s) on LCD screen
+    lcd_gotoxy(6, 1);
     lcd_puts("LCD Test");
     lcd_putc('!');
 
-    // Configure 8-bit Timer/Counter2 for Stopwatch
+    // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
 
 
@@ -44,31 +62,38 @@ int main(void)
     while (1)
     {
         /* Empty loop. All subsequent operations are performed exclusively 
-         * inside interrupt service routines ISRs */
+         * inside interrupt service routines, ISRs */
     }
 
     // Will never reach this
     return 0;
 }
 
+
 /* Interrupt service routines ----------------------------------------*/
 /**********************************************************************
  * Function: Timer/Counter2 overflow interrupt
- * Purpose:  Update the stopwatch on LCD display every sixth overflow,
+ * Purpose:  Update the stopwatch on LCD screen every sixth overflow,
  *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
  **********************************************************************/
 ISR(TIMER2_OVF_vect)
 {
-    static uint8_t number_of_overflows = 0;
+    static uint8_t no_of_overflows = 0;
+    static uint8_t tenths = 0;  // Tenths of a second
+    char string[2];             // String for converted numbers by itoa()
 
-    number_of_overflows++;
-    if (number_of_overflows >= 6)
+    no_of_overflows++;
+    if (no_of_overflows >= 6)
     {
         // Do this every 6 x 16 ms = 100 ms
-        number_of_overflows = 0;
+        no_of_overflows = 0;
 
-        // WRITE YOUR CODE HERE
+        // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
 
+
+        itoa(tenths, string, 10);  // Convert decimal value to string
+        lcd_gotoxy(7, 0);
+        lcd_puts(string);
     }
     // Else do nothing and exit the ISR
 }
