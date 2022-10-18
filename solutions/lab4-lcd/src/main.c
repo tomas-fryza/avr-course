c870cc08077c4361f822063e85014b271fb556a6
tomas-fryza <tomas.fryza@gmail.com>
2022-10-18 14:32:02 +0200

    Custom characters added

--- solutions/lab4-lcd/src/main.c
+++ solutions/lab4-lcd/src/main.c
@@ -1,7 +1,7 @@
 /***********************************************************************
  * 
  * Stopwatch by Timer/Counter2 on the Liquid Crystal Display (LCD)
-
+ *
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
  * Copyright (c) 2017 Tomas Fryza
@@ -21,7 +21,7 @@
  *     D5   - PD5 (Data bit 5)
  *     D6   - PD6 (Data bit 3)
  *     D7   - PD7 (Data bit 2)
- *     A+K  - Back-light enabled by PB2
+ *     A+K  - Back-light enabled/disabled by PB2
  * 
  **********************************************************************/
 
@@ -43,27 +43,65 @@
  **********************************************************************/
 int main(void)
 {
+    uint8_t customChar[16] = {
+        // First character
+        0b11100,
+        0b01110,
+        0b00111,
+        0b00011,
+        0b00111,
+        0b01110,
+        0b11100,
+        0b11000,
+        // Second character
+        0b00011,
+        0b00111,
+        0b01110,
+        0b11100,
+        0b11100,
+        0b01110,
+        0b00111,
+        0b00011
+    };
+
     // Initialize LCD display
     lcd_init(LCD_DISP_ON_CURSOR);
+
+    // Set pointer to beginning of CGRAM memory
+    lcd_command(1 << LCD_CGRAM);
+    for (uint8_t i = 0; i < 16; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar[i]);
+    }
+    // Set DDRAM address to display characters
+    lcd_command(1 << LCD_DDRAM);
 
     // Put string(s) on LCD screen
     // lcd_gotoxy(6, 1);
     // lcd_puts("LCD Test");
     // lcd_putc('!');
-    lcd_gotoxy(1,0);
+    lcd_gotoxy(1, 0);
     lcd_puts("00:00.0");
 
-    lcd_gotoxy(11,0);
-    lcd_puts("a");
+    lcd_gotoxy(11, 0);
+    lcd_putc('a');
 
-    lcd_gotoxy(1,1);
+    lcd_gotoxy(1, 1);
     lcd_puts("b");
 
-    lcd_gotoxy(11,1);
+    lcd_gotoxy(11, 1);
     lcd_puts("c");
 
-    // GPIO_mode_output(&DDRB, PB2);
+    // Two custom characters
+    lcd_gotoxy(13, 1);
+    lcd_putc(0x00);
+    lcd_putc(0x01);
+
+    // Set back light at PB2
+    GPIO_mode_output(&DDRB, PB2);
     // GPIO_write_low(&PORTB, PB2);
+    GPIO_write_high(&PORTB, PB2);
 
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
@@ -95,8 +133,8 @@ ISR(TIMER2_OVF_vect)
 {
     static uint8_t no_of_overflows = 0;
     static uint8_t tenths = 0;  // Tenths of a second
-    char string[2];             // String for converted numbers by itoa()
     static uint8_t secs = 0;    // Seconds
+    char string[2];             // String for converted numbers by itoa()
 
     no_of_overflows++;
     if (no_of_overflows >= 6)
@@ -109,6 +147,7 @@ ISR(TIMER2_OVF_vect)
         if (tenths > 9)
         {
             tenths = 0;
+            // Add seconds to stopwatch
             secs++;
             if (secs > 59)
             {
@@ -118,12 +157,10 @@ ISR(TIMER2_OVF_vect)
             itoa(secs, string, 10);
             lcd_gotoxy(4, 0);
             if (secs < 10)
-            {
-                lcd_puts("0");
-            }
+                lcd_putc('0');
             lcd_puts(string);
         }
-        // Display tenths of a second
+        // Show tenths of a second on LCD screen
         itoa(tenths, string, 10);  // Convert decimal value to string
         lcd_gotoxy(7, 0);
         lcd_puts(string);
