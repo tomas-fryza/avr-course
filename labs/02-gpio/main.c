4c901ae6690db018d44c2441db8d7cd4276cc746
Tomas Fryza <tomas.fryza@gmail.com>
2022-11-04 11:25:12 +0100

    Added comments about versions

--- labs/02-gpio/main.c
+++ labs/02-gpio/main.c
@@ -1,8 +1,9 @@
 /***********************************************************************
  * 
- * Alternately toggle two LEDs when a push button is pressed. Use 
- * functions from GPIO library.
- * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ * Blink two LEDs in Arduino-, register-, and library-style. Use 
+ * function from delay library to slow down blinking.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
  * Copyright (c) 2019 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
@@ -10,44 +11,70 @@
  * 
  **********************************************************************/
 
+
 /* Defines -----------------------------------------------------------*/
-#define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define BLINK_DELAY 500
+#define LED_GREEN PB5   // PB5 is AVR pin where green on-board LED
+                        // is connected
+#define LED_RED PB0     // External active-low LED
+#define SHORT_DELAY 250 // Delay in milliseconds
 #ifndef F_CPU
-# define F_CPU 16000000     // CPU frequency in Hz required for delay
+# define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
 #endif
 
+
 /* Includes ----------------------------------------------------------*/
-#include <util/delay.h>     // Functions for busy-wait delay loops
-#include <avr/io.h>         // AVR device-specific IO definitions
-#include "gpio.h"           // GPIO library for AVR-GCC
+#include <avr/io.h>     // AVR device-specific IO definitions
+#include <util/delay.h> // Functions for busy-wait delay loops
+#include <gpio.h>       // GPIO library for AVR-GCC
+
+
+// -----
+// This part is needed to use Arduino functions but also physical pin
+// names. We are using Arduino-style just to simplify the first lab.
+#include "Arduino.h"
+#define PB5 13          // In Arduino world, PB5 is called "13"
+#define PB0 8
+// -----
+
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Toggle two LEDs when a push button is pressed. Functions 
- *           from user-defined GPIO library is used.
+ * Purpose:  Toggle LEDs and use delay library.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
-    // Green LED at port B
-    GPIO_config_output(&DDRB, LED_GREEN);
-    GPIO_write_low(&PORTB, LED_GREEN);
+    uint8_t led_value = 0;  // Local variable to keep LED status
 
-    // Configure the second LED at port C
+    // Set pins where LEDs are connected as output
+    // Ver 1: Arduino style
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
 
+    // Ver 2: Low-level (register) style
 
-    // Configure Push button at port D and enable internal pull-up resistor
-
+    // Ver 3: Library function style
 
     // Infinite loop
     while (1)
     {
         // Pause several milliseconds
-        _delay_ms(BLINK_DELAY);
+        _delay_ms(SHORT_DELAY);
 
-        // WRITE YOUR CODE HERE
+        // Change LED value
+        if (led_value == 0) {
+            led_value = 1;
+            // Set pin(s) to HIGH
+            digitalWrite(LED_GREEN, HIGH);
+            digitalWrite(LED_RED, HIGH);
+        }
+        else {
+            led_value = 0;
+            // Clear pin(s) to LOW
+            digitalWrite(LED_GREEN, LOW);
+            digitalWrite(LED_RED, LOW);
+        }
     }
 
     // Will never reach this
