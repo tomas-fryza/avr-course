3f6ffc106c804c413281fd3577bb8d2363edc8ba
Tomas Fryza <48966045+tomas-fryza@users.noreply.github.com>
2023-09-25 21:38:42 +0200

    Create 01_variables.c

---
+++ c/01_variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>    // Standard C input/output library
+#include <stdint.h>   // Standard C integer library
+
+int main(void)        // Main function with no input parameters
+{                     // Beginning of function body
+    int8_t  a = 200;  // Definition of two local variables
+    uint8_t b = 200;
+                      // Print formated string to a Terminal
+    printf("Print int8 value: %d\n", a);  // Integer `a` will be printed to `%d`
+    printf("Print uint8 value: %d\n", b); // Formatting char. `\n` inserts new line
+
+    return 0;         // Return value of main function
+}                     // End of function body
