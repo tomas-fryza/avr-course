e517f82cb0de06e0d6fa34123a88b39891864aee
Tomas Fryza <48966045+tomas-fryza@users.noreply.github.com>
2023-09-25 21:46:49 +0200

    Update 01_variables.c

--- c/01_variables.c
+++ c/01_variables.c
@@ -8,6 +8,7 @@ int main(void)        // Main function with no input parameters
                       // Print formated string to a Terminal
     printf("Print int8 value: %d\n", a);  // Integer `a` will be printed to `%d`
     printf("Print uint8 value: %d\n", b); // Formatting char. `\n` inserts new line
+    printf("Size of uint8: %d B\n", sizeof(uint8_t));
 
     return 0;         // Return value of main function
 }                     // End of function body
