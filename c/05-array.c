66dd066b214988933d389612bb861dbd12b67436
Tomas Fryza <48966045+tomas-fryza@users.noreply.github.com>
2023-09-26 10:07:18 +0200

    Create 05-array.c

---
+++ c/05-array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>   // Standard I/O library for 'printf'
+#include <stdint.h>  // Standard C integer library
+
+int main(void)
+{
+    // Declare and initialize a zero integer array
+    uint8_t values[5] = {0};  // {0, 0, 0, 0, 0}
+
+    // Modify elements of the array
+    for (uint8_t i = 0; i < 5; i++) {
+        values[i] = i*i + 1;  // {1, 2, 5, 10, 17}
+    }
+
+    // Print the modified array
+    printf("Modified array:\n");
+    for (uint8_t i = 0; i < 5; i++) {
+        printf("[%d] %d\n", i,values[i]);
+    }
+
+    return 0;
+}
