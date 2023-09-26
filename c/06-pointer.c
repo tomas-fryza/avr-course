de33709590cefeeb40feb75fc9373510462dc184
Tomas Fryza <48966045+tomas-fryza@users.noreply.github.com>
2023-09-26 12:22:46 +0200

    Update 06-pointer.c

--- c/06-pointer.c
+++ c/06-pointer.c
@@ -7,17 +7,16 @@ int main(void)
     // Declare and initialize a zero integer array
     uint8_t values[SIZE] = {0};  // {0, 0, 0, 0, 0}
     // Point `pValues` to the start of the 'values' array
-    uint8_t *pValues = values;   // Same as *pValues = &values[0]
+    uint8_t *pValues = values;   // *pValues = &values[0]
 
     // Modify elements of the array
     for (uint8_t i = 0; i < SIZE; i++) {
-        *(pValues + i) = i*i + 1;  // {1, 2, 5, 10, 17}
+        *(pValues + i) = i*i + 1;// {1, 2, 5, 10, 17, ...}
     }
 
-    // Print the modified array
-    printf("Modified array:\n");
+    printf("Modified array:\n"); // Print modified array
     for (uint8_t i = 0; i < SIZE; i++) {
-        printf("[%d] %d\n", i,*pValues++);
+        printf("[%d] %p: %d\n", i,pValues,*pValues++);
     }
 
     return 0;
