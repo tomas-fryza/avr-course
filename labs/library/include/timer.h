3b204f5d2f462236523c05e58948f1d89a314065
Tomas Fryza <48966045+tomas-fryza@users.noreply.github.com>
2023-10-09 09:27:32 +0200

    Directives renamed

--- labs/library/include/timer.h
+++ labs/library/include/timer.h
@@ -42,22 +42,22 @@
  * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 16, F_CPU = 16 MHz
  */
 /** @brief Stop timer, prescaler 000 --> STOP */
-#define TIM1_stop()           TCCR1B &= ~((1<<CS12) | (1<<CS11) | (1<<CS10));
+#define TIM1_STOP      TCCR1B &= ~((1<<CS12) | (1<<CS11) | (1<<CS10));
 /** @brief Set overflow 4ms, prescaler 001 --> 1 */
-#define TIM1_overflow_4ms()   TCCR1B &= ~((1<<CS12) | (1<<CS11)); TCCR1B |= (1<<CS10);
+#define TIM1_OVF_4MS   TCCR1B &= ~((1<<CS12) | (1<<CS11)); TCCR1B |= (1<<CS10);
 /** @brief Set overflow 33ms, prescaler 010 --> 8 */
-#define TIM1_overflow_33ms()  TCCR1B &= ~((1<<CS12) | (1<<CS10)); TCCR1B |= (1<<CS11);
+#define TIM1_OVF_33MS  TCCR1B &= ~((1<<CS12) | (1<<CS10)); TCCR1B |= (1<<CS11);
 /** @brief Set overflow 262ms, prescaler 011 --> 64 */
-#define TIM1_overflow_262ms() TCCR1B &= ~(1<<CS12); TCCR1B |= (1<<CS11) | (1<<CS10);
+#define TIM1_OVF_262MS TCCR1B &= ~(1<<CS12); TCCR1B |= (1<<CS11) | (1<<CS10);
 /** @brief Set overflow 1s, prescaler 100 --> 256 */
-#define TIM1_overflow_1s()    TCCR1B &= ~((1<<CS11) | (1<<CS10)); TCCR1B |= (1<<CS12);
+#define TIM1_OVF_1SEC  TCCR1B &= ~((1<<CS11) | (1<<CS10)); TCCR1B |= (1<<CS12);
 /** @brief Set overflow 4s, prescaler // 101 --> 1024 */
-#define TIM1_overflow_4s()    TCCR1B &= ~(1<<CS11); TCCR1B |= (1<<CS12) | (1<<CS10);
+#define TIM1_OVF_4SEC  TCCR1B &= ~(1<<CS11); TCCR1B |= (1<<CS12) | (1<<CS10);
 
 /** @brief Enable overflow interrupt, 1 --> enable */
-#define TIM1_overflow_interrupt_enable()  TIMSK1 |= (1<<TOIE1);
+#define TIM1_OVF_ENABLE  TIMSK1 |= (1<<TOIE1);
 /** @brief Disable overflow interrupt, 0 --> disable */
-#define TIM1_overflow_interrupt_disable() TIMSK1 &= ~(1<<TOIE1);
+#define TIM1_OVF_DISABLE TIMSK1 &= ~(1<<TOIE1);
 
 
 /**
