75119f1243ba4ddb81548de8ad901fcfd38108fe
tomas-fryza <tomas.fryza@gmail.com>
2024-11-04 16:13:44 +0100

    Lib moved to new folder

---
+++ library/segment/segment.c
@@ -0,0 +1,106 @@
+/***********************************************************************
+ * 
+ * Seven-segment display library for AVR-GCC.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2019 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+
+/* Includes ----------------------------------------------------------*/
+#ifndef F_CPU
+# define F_CPU 16000000
+#endif
+#include <util/delay.h>
+#include "gpio.h"
+#include "segment.h"
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: SEG_init()
+ * Purpose:  Configure SSD signals LATCH, CLK, and DATA as output.
+ * Returns:  none
+ **********************************************************************/
+void SEG_init(void)
+{
+    /* Configuration of SSD signals */
+    GPIO_config_output(&DDRD, SEG_LATCH);
+    GPIO_config_output(&DDRD, SEG_CLK);
+    GPIO_config_output(&DDRB, SEG_DATA);
+}
+
+
+/**********************************************************************
+ * Function: SEG_update_shift_regs()
+ * Purpose:  Display segments at one position of the SSD.
+ * Input:    segments - Segments to be displayed (abcdefgDP, active low)
+ *           position - Position of the display where the segments are to 
+ *                      be displayed (p3 p2 p1 p0 xxxx, active high)
+ * Returns:  none
+ **********************************************************************/
+void SEG_update_shift_regs(uint8_t segments, uint8_t position)
+{
+    uint8_t bit_number;
+
+    // Pull LATCH, CLK, and DATA low
+
+    // Wait 1 us
+
+    // Loop through the 1st byte (segments)
+    // a b c d e f g DP (active low values)
+    for (bit_number = 0; bit_number < 8; bit_number++)
+    {
+        // Test LSB of "segments" by & (faster) or % (slower) and... 
+        // ...output DATA value
+
+        // Wait 1 us
+
+        // Pull CLK high
+
+        // Wait 1 us
+
+        // Pull CLK low
+
+        // Shift "segments"
+        segments = segments >> 1;
+    }
+
+    // Loop through the 2nd byte (position)
+    // p3 p2 p1 p0 . . . . (active high values)
+    for (bit_number = 0; bit_number < 8; bit_number++)
+    {
+        // Test LSB of "position" by & (faster) or % (slower) and... 
+        // ...output DATA value
+
+        // Wait 1 us
+
+        // Pull CLK high
+
+        // Wait 1 us
+
+        // Pull CLK low
+
+        // Shift "position"
+        position = position >> 1;
+    }
+
+    // Pull LATCH high
+
+    // Wait 1 us
+
+}
+
+
+/**********************************************************************
+ * Function: SEG_clear()
+ **********************************************************************/
+
+
+/**********************************************************************
+ * Function: SEG_clk_2us()
+ **********************************************************************/
