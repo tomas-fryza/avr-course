0b4fcc843a711ee6c07a8d4fd7f4a86910a68d31
tomas-fryza <tomas.fryza@gmail.com>
2024-11-04 16:38:42 +0100

    Comments and defines updated

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -1,34 +1,30 @@
-/***********************************************************
- * 
- * Blink a LED using register approach.
+/*
+ * Blink a LED using delay.
  * (c) 2018-2024 Tomas Fryza, MIT license
  *
  * Developed using PlatformIO and AVR 8-bit Toolchain 3.6.2.
  * Tested on Arduino Uno board and ATmega328P, 16 MHz.
- * 
- ***********************************************************/
+ */
 
-/* Defines ------------------------------------------------*/
-#define LED_GREEN PB5   // AVR pin where green LED is connected
-#define SHORT_DELAY 250 // Delay in milliseconds
-#ifndef F_CPU
-# define F_CPU 16000000 // CPU frequency in Hz required for delay
-#endif
-
-/* Includes -----------------------------------------------*/
-#include <avr/io.h>     // AVR device-specific IO definitions
-#include <util/delay.h> // Functions for busy-wait delay loops
+// -- Defines --------------------------------------------------------
+#define LED_BUILTIN PB5  // On-board LED
+#define SHORT_DELAY 250  // Delay in milliseconds
 
 
-/* Function definitions -----------------------------------*/
-/***********************************************************
+// -- Includes -------------------------------------------------------
+#include <avr/io.h>      // AVR device-specific IO definitions
+#include <util/delay.h>  // Functions for busy-wait delay loops
+
+
+// -- Function definitions -------------------------------------------
+/*
  * Function: Main function where the program execution begins
- * Purpose:  Toggle one LED and use delay library.
+ * Purpose:  Toggle LED using delay.
  * Returns:  none
- ***********************************************************/
+ */
 int main(void)
 {
-    // Set pin as output in Data Direction Register
+    // Set output pin in Data Direction Register
     DDRB = DDRB | (1 << LED_GREEN);
     // Set pin LOW in Data Register (LED off)
     PORTB = PORTB & ~(1 << LED_GREEN);
